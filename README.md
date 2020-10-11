66f5d87b0b5fd2f3a66996dbd7c3ab85b431ac57
Patrick Bello <mayorbello@yahoo.com>
2020-10-10 20:11:41 -0600

    Fixed Hamburger Menu + Updated Readme

--- README.md
+++ README.md
@@ -7,4 +7,6 @@ Contact form from [Formspree](https://formspree.io/)
 
 ### [Github Pages](https://mayorbcode.github.io/cpnt260-final/)  
   
-Can't seem to be able to get the hamburger menu to actually work so will just wait till we cover it in class.
+Can't seem to be able to get the hamburger menu to actually work so will just wait till we cover it in class.  
+  
+Edit: It turns out CSS was the culprit all along. Specificity to be specific (Get it?). Starting a petition for it to be called CBS (Cascading Bullsh*t)!
--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
-const button = document.querySelector('.main-nav .btn');
+const button = document.querySelector('.btn');
 
-const menu = document.querySelector('.main-nav li');
+const menu = document.querySelector('.main-nav ul');
+
+const home = document.querySelector('.main-nav ul li:nth-child(1)');
+
+const exitButton = document.querySelector('.exit');
 
 function eventHandler() {
   menu.classList.toggle('visible');
+  home.classList.toggle('visible');
+  exitButton.classList.toggle('visible');
+  button.classList.toggle('hide');
 }
 
 button.addEventListener('click', eventHandler);
+exitButton.addEventListener('click', eventHandler);
