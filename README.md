a753fddb5b848fdfe941697e12077a605dd3c5d3
Patrick Bello <mayorbello@yahoo.com>
2020-10-12 14:06:55 -0600

    added comments to JS file + added comments to README

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Contact form from [Formspree](https://formspree.io/)
 [Tony's hamburger toggle sample code](https://github.com/sait-wbdv/sample-code/tree/master/frontend/hamburger-toggle)   
 
 ### [Github Pages](https://mayorbcode.github.io/cpnt260-final/)  
-  
+## Comments 
 Can't seem to be able to get the hamburger menu to actually work so will just wait till we cover it in class.  
   
 Edit: It turns out CSS was the culprit all along. Specificity to be specific (Get it?). Starting a petition for it to be called CBS (Cascading Bullsh*t)!
--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Define Variables
 const button = document.querySelector('.btn');
 
 const menu = document.querySelector('.main-nav ul');
@@ -6,12 +7,16 @@ const home = document.querySelector('.main-nav ul li:nth-child(1)');
 
 const exitButton = document.querySelector('.exit');
 
+// Declare function
 function eventHandler() {
+  // Make these visible on click
   menu.classList.toggle('visible');
   home.classList.toggle('visible');
   exitButton.classList.toggle('visible');
+  // Hide on click
   button.classList.toggle('hide');
 }
 
+// Invoke functions
 button.addEventListener('click', eventHandler);
 exitButton.addEventListener('click', eventHandler);
